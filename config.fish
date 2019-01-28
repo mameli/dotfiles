7cfe736641e85f0475525c34eff1d9def5f32d3a
mameli <mameli93@gmail.com>
2019-01-28 09:46:18 +0100

    fish conda virtual env

--- config.fish
+++ config.fish
@@ -6,6 +6,8 @@ function lastcommit
 	git log -1
 end
 
-set PATH /snap/bin $PATH
-set PATH ~/anaconda3/bin $PATH
+set PATH /opt/miniconda3/bin $PATH 
+# Virtual env
+source /opt/miniconda3/etc/fish/conf.d/conda.fish
+
 set -gx TERM screen-256color-bce
